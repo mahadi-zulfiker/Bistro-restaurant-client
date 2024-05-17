5268a709c14e2a7778515ec6ba429a5f17d6b3bf
mahade <mahade.adib45@gmail.com>
2024-05-17 21:37:12 +0600

    menu page basic setup done

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from './Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -6,7 +9,12 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>menu</h2>
+            <Cover img={menuImg} title={"our menu"}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title={"our menu"}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title={"our menu"}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
