97bc75de8f02d45e5b51379293e1531caaec0a47
mahade <mahade.adib45@gmail.com>
2024-05-18 11:49:17 +0600

    coustom hooks added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from './Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from '../../Home/Home/PopularMenu/PopularMenu';
+
 
 const Menu = () => {
     return (
@@ -10,11 +10,6 @@ const Menu = () => {
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImg} title={"our menu"}></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title={"our menu"}></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title={"our menu"}></Cover>
-            <PopularMenu></PopularMenu>
         </div>
     );
 };
