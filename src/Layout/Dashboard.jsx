e985e79627f4a71681082752723bde92795284c6
mahade <mahade.adib45@gmail.com>
2024-05-24 16:44:56 +0600

    cart delete function added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,8 +1,10 @@
 import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../Hooks/useCart";
 
 
 const Dashboard = () => {
+    const [cart] = useCart();
     return (
         <div className="flex">
             <div className="w-64 min-h-screen bg-orange-400">
@@ -14,7 +16,7 @@ const Dashboard = () => {
                         <NavLink to="/dashboard/reservation"><FaCalendar></FaCalendar>Reservation</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/dashboard/cart"><FaShoppingCart></FaShoppingCart>My Cart</NavLink>
+                        <NavLink to="/dashboard/cart"><FaShoppingCart></FaShoppingCart>My Cart ({cart.length})</NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/review"><FaAd></FaAd>Add Reviews</NavLink>
