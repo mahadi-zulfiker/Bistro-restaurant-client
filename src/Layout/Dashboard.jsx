0c0da97498249e343e471b8632962ca6b1320585
mahade <mahade.adib45@gmail.com>
2024-05-25 10:28:31 +0600

    admin panel added and all users pages added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,35 +1,64 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaBook, FaCalendar, FaHome, FaList, FaPhone, FaSearch, FaShoppingCart, FaUsers, FaUtensils } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../Hooks/useCart";
 
 
 const Dashboard = () => {
     const [cart] = useCart();
+
+    const isAdmin = true;
+
+
     return (
         <div className="flex">
             <div className="w-64 min-h-screen bg-orange-400">
                 <ul className="menu p-4 text-white">
-                    <li>
-                        <NavLink to="/dashboard/userHome"><FaHome></FaHome>User Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/dashboard/reservation"><FaCalendar></FaCalendar>Reservation</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/dashboard/cart"><FaShoppingCart></FaShoppingCart>My Cart ({cart.length})</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/dashboard/review"><FaAd></FaAd>Add Reviews</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/dashboard/bookings"><FaList></FaList>My Bookings</NavLink>
-                    </li>
+                    {
+                        isAdmin ? <>
+                            <li>
+                                <NavLink to="/dashboard/adminHome"><FaHome></FaHome>Admin Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/addItems"><FaUtensils></FaUtensils>Add Items</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/manageItems"><FaList></FaList>Manage Items</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/bookings"><FaBook></FaBook>Manage Bookings</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/users"><FaUsers></FaUsers>All Users</NavLink>
+                            </li>
+                        </> :
+                            <>
+                                <li>
+                                    <NavLink to="/dashboard/userHome"><FaHome></FaHome>User Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/reservation"><FaCalendar></FaCalendar>Reservation</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/cart"><FaShoppingCart></FaShoppingCart>My Cart ({cart.length})</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/review"><FaAd></FaAd>Add Reviews</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/bookings"><FaList></FaList>My Bookings</NavLink>
+                                </li>
+                            </>
+                    }
+                    {/* shared nav links */}
                     <div className="divider">-------------</div>
                     <li>
                         <NavLink to="/"><FaHome></FaHome>Home</NavLink>
                     </li>
                     <li>
                         <NavLink to="/order/salad"><FaSearch></FaSearch>menu</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/order/contact"><FaPhone></FaPhone>Contact</NavLink>
                     </li>
                 </ul>
             </div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -11,6 +11,7 @@ import Secret from "../Pages/Home/Home/Shared/Secret/Secret";
 import PrivateRoute from "./PrivateRoute";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../Pages/Dashboard/Cart/Cart";
+import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 
 export const router = createBrowserRouter([
     {
@@ -54,6 +55,12 @@ export const router = createBrowserRouter([
             {
                 path: 'cart',
                 element: <Cart></Cart>
+            },
+
+            // admin routes
+            {
+                path: 'users',
+                element: <AllUsers></AllUsers>
             },
         ]
     }
