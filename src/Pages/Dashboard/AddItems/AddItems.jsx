5a1f147b7fd7a42a3b88e97941d7b39ce508881b
mahade <mahade.adib45@gmail.com>
2024-05-26 16:38:40 +0600

    add items function added to add data to database

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -2,24 +2,51 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, reset } = useForm();
     const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
     const onSubmit = async (data) => {
         console.log(data)
         // image upload to imgbb and then get an url
-        const imageFile = {image: data.image[0]}
+        const imageFile = { image: data.image[0] }
         const res = await axiosPublic.post(image_hosting_api, imageFile, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
         });
-        console.log(res.data);
+        if (res.data.success) {
+            //now send the menu item data to the server with the image
+            const menuItem = {
+                name: data.name,
+                category: data.category,
+                price: parseFloat(data.price),
+                recipe: data.recipe,
+                image: res.data.data.display_url
+            }
+            //
+            const menuRes = await axiosSecure.post('/menu', menuItem);
+            console.log(menuRes.data);
+            if (menuRes.data.insertedId) {
+                //
+                reset();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${data.name} is added to the menu.`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        }
+        //console.log(res.data);
     };
 
     return (
@@ -31,7 +58,7 @@ const AddItems = () => {
                         <div className="label">
                             <span className="label-text">Recipe Name*</span>
                         </div>
-                        <input {...register('name', {required: true})}
+                        <input {...register('name', { required: true })}
                             required
                             type="text" placeholder="Recipe Name" className="input input-bordered w-full" />
                     </label>
@@ -41,7 +68,7 @@ const AddItems = () => {
                             <div className="label">
                                 <span className="label-text">Category*</span>
                             </div>
-                            <select defaultValue="default" {...register('category', {required: true})}
+                            <select defaultValue="default" {...register('category', { required: true })}
                                 className="select select-bordered w-full">
                                 <option disabled value="default">Select a category</option>
                                 <option value="salad">Salad</option>
@@ -57,7 +84,7 @@ const AddItems = () => {
                             <div className="label">
                                 <span className="label-text">Price*</span>
                             </div>
-                            <input {...register('price', {required: true})}
+                            <input {...register('price', { required: true })}
                                 type="number" placeholder="price" className="input input-bordered w-full" />
                         </label>
                     </div>
@@ -69,7 +96,7 @@ const AddItems = () => {
                         <textarea {...register('recipe')} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
                     </label>
                     <div className="form-control w-full my-5">
-                        <input {...register('image', {required: true})} type="file" className="file-input w-full max-w-xs" />
+                        <input {...register('image', { required: true })} type="file" className="file-input w-full max-w-xs" />
                     </div>
                     <button className="btn">
                         Add Item <FaUtensils></FaUtensils>
