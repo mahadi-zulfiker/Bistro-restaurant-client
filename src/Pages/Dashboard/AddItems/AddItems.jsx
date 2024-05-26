f757d3363e8425625e1ce76e6cc92d813ef73733
mahade <mahade.adib45@gmail.com>
2024-05-26 11:57:33 +0600

    add item from added

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
 
 
 const AddItems = () => {
@@ -14,17 +15,53 @@ const AddItems = () => {
             <SectionTitle heading={"add an item"} subHeading={"What's New ?"}></SectionTitle>
             <div>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register("name")} />
-                    <select {...register('category')}
-                    className="select select-bordered w-full max-w-xs">
-                        <option disabled selected>Select a category</option>
-                        <option value="salad">Salad</option>
-                        <option value="pizza">Pizza</option>
-                        <option value="soup">Soup</option>
-                        <option value="dessert">Dessert</option>
-                        <option value="drinks">Drinks</option>
-                    </select>
-                    <input type="submit" />
+                    <label className="form-control w-full my-6">
+                        <div className="label">
+                            <span className="label-text">Recipe Name*</span>
+                        </div>
+                        <input {...register('name', {required: true})}
+                            required
+                            type="text" placeholder="Recipe Name" className="input input-bordered w-full" />
+                    </label>
+                    <div className="flex gap-6">
+                        {/* category */}
+                        <label className="form-control w-full my-6">
+                            <div className="label">
+                                <span className="label-text">Category*</span>
+                            </div>
+                            <select {...register('category', {required: true})}
+                                className="select select-bordered w-full">
+                                <option disabled selected>Select a category</option>
+                                <option value="salad">Salad</option>
+                                <option value="pizza">Pizza</option>
+                                <option value="soup">Soup</option>
+                                <option value="dessert">Dessert</option>
+                                <option value="drinks">Drinks</option>
+                            </select>
+                        </label>
+
+                        {/* price */}
+                        <label className="form-control w-full my-6">
+                            <div className="label">
+                                <span className="label-text">Price*</span>
+                            </div>
+                            <input {...register('price', {required: true})}
+                                type="number" placeholder="price" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    {/* recipe details */}
+                    <label className="form-control">
+                        <div className="label">
+                            <span className="label-text">Recipe Details</span>
+                        </div>
+                        <textarea {...register('recipe')} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+                    </label>
+                    <div className="form-control w-full my-5">
+                        <input {...register('image', {required: true})} type="file" className="file-input w-full max-w-xs" />
+                    </div>
+                    <button className="btn">
+                        Add Item <FaUtensils></FaUtensils>
+                    </button>
                 </form>
             </div>
         </div>
