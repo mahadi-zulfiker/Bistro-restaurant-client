427a04b51d6b2465ec2d3c16f9a81cd158bc29dd
mahade <mahade.adib45@gmail.com>
2024-05-28 16:05:39 +0600

    payment method added using stripe

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,12 +1,18 @@
+import { Elements } from "@stripe/react-stripe-js";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import {loadStripe} from '@stripe/stripe-js';
+import CheckOutFrom from "./CheckOutFrom";
 
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
 
 const Payment = () => {
     return (
         <div>
             <SectionTitle heading={"Payment"} subHeading={"Please Pay to eat"}></SectionTitle>
             <div>
-                
+                <Elements stripe={stripePromise}>
+                    <CheckOutFrom></CheckOutFrom>
+                </Elements>
             </div>
         </div>
     );
