222231179ad380d8671b46bcd02767da36bd1572
mahade <mahade.adib45@gmail.com>
2024-06-14 20:39:43 +0600

    project done

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,13 +1,14 @@
 import { FaAd, FaBook, FaCalendar, FaHome, FaList, FaPhone, FaSearch, FaShoppingCart, FaUsers, FaUtensils } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../Hooks/useCart";
+import useAdmin from "../Hooks/useAdmin";
 //import useAdmin from "../Hooks/useAdmin";
 
 
 const Dashboard = () => {
     const [cart] = useCart();
 
-    const [isAdmin] = true;
+    const [isAdmin] = useAdmin();
 
 
     return (
